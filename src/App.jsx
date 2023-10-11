854bc1a88b111901cc3992dd74bde79f15016a76
RafaelLSuarez1997 <63333102+RafaelLSuarez1997@users.noreply.github.com>
2023-10-11 15:04:10 -0400

    added circles

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 // Write your Color component here
+const Color = (props) => {
+  return <div className={props.color}></div>
+}
 
 const App = () => {
   return (
     <div id="container">
+      <div id="colors-list">
+      <Color color="red"/>
+      <Color color="violet"/>
+      <Color color="green"/>
+      </div>
       <div id="navbar">
         <div>Currently selected: </div>
         <div className="red">red</div>
-        <h1>hello world</h1>
       </div>
       <div id="colors-list">{/* colors go here */}</div>
     </div>
