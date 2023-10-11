693e2bec04eb33d4f1bd65b30a166b72866426c2
RafaelLSuarez1997 <63333102+RafaelLSuarez1997@users.noreply.github.com>
2023-10-11 14:53:17 -0400

    testing git

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ const App = () => {
       <div id="navbar">
         <div>Currently selected: </div>
         <div className="red">red</div>
+        <h1>hello world</h1>
       </div>
       <div id="colors-list">{/* colors go here */}</div>
     </div>
